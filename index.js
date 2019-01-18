5c9a247804b594c84730794010e33e383fc80aeb
Tshili <Tshili@mbp-de-tshili.home>
2019-01-18 23:08:32 +0100

    conf

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
+  res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
   res.send('deployement ok');
   bot();
   setInterval(bot, cnf.settings.run_every_x_hours * 3600000);
