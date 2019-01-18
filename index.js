13d8e05393c544142cfa3243d4032484c2485ecd
Tshili <Tshili@mbp-de-tshili.home>
2019-01-18 21:29:32 +0100

    express

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 let bot = require('./src/puppeteer');
 let cnf = require('./config/config.json');
 
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+  res.send('deployement ok');
+  bot();
+  setInterval(bot, cnf.settings.run_every_x_hours * 3600000);
+});
 
+app.listen(port);
 
+//bot();
 
-bot();
-
-setInterval(bot, cnf.settings.run_every_x_hours * 3600000);
+//setInterval(bot, cnf.settings.run_every_x_hours * 3600000);
